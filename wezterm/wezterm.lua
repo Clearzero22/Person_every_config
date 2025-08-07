69b712a583f8f2286ef8600e698cfd526b691e7a
Clearzero22 <2541330269@qq.com>
2025-08-07 20:09:17 +0800

    add doc file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 -- WezTerm configuration
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- For example, you can set the color scheme
-config.color_scheme = 'tokyonight'
+config.color_scheme = "tokyonight"
 
 return config
+
+-- TODO 开发新的功能
